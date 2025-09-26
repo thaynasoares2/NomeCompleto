daa793de6d2bd6794d84a63811f4691436a5f618
Thayná <thaynasoares2123@gmail.com>
2025-09-26 16:33:45 -0300

    FinalizarExercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.Write("Thayná");
+nome = Console.ReadLine()!;
+
+Console.Write("Soares");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome Completo: Thayná Soares {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: SOARES, Thayná {sobrenome.ToUpper()}, {nome}");
